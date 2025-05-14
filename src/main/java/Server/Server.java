179015f40a435d896bb77457dbfb62b577311321
Mehrdad Shirvani <mehrdadsh0901@gmail.com>
2025-05-14 11:28:44 +0330

    chore: add TODOs in Server

--- src/main/java/Server/Server.java
+++ src/main/java/Server/Server.java
@@ -1,20 +1,32 @@
 package Server;
-
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.concurrent.CopyOnWriteArrayList;
-
+import java.util.ArrayList;
+import Shared.User;
 public class Server {
-    //TODO?????? CopyOnWriteArrayList
-    public static CopyOnWriteArrayList<ClientHandler> clients = new CopyOnWriteArrayList<>();
+    private static final User[] users = {
+            new User("user1", "1234"),
+            new User("user2", "1234"),
+            new User("user3", "1234"),
+            new User("user4", "1234"),
+            new User("user5", "1234"),
+    };
+
+    public static ArrayList<ClientHandler> clients = new ArrayList<>();
 
     public static void main(String[] args) throws Exception {
-        ServerSocket serverSocket = new ServerSocket(12345);
-        System.out.println("Server running on port 12345...");
+        //TODO: start server socket
 
-        while (true) {
-            Socket clientSocket = serverSocket.accept();
-            //...
+        //TODO: accept clients and create a thread for each
+        //TODO: update the list of clients
+    }
+
+    public static boolean login(String username, String password) {
+        for (User user : users) {
+            if (user.getUsername().equals(username) && user.getPassword().equals(password)) {
+                return true;
+            }
         }
+        return false;
     }
 }
