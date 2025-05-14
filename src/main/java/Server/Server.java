78891766bb56b9f33ecd5af8561c66c5b5b60d6c
Mehrdad Shirvani <mehrdadsh0901@gmail.com>
2025-05-14 20:39:20 +0330

    chore: improve clarity of TODO comments in Server

--- src/main/java/Server/Server.java
+++ src/main/java/Server/Server.java
@@ -4,6 +4,7 @@ import java.net.Socket;
 import java.util.ArrayList;
 import Shared.User;
 public class Server {
+    // Predefined users for authentication
     private static final User[] users = {
             new User("user1", "1234"),
             new User("user2", "1234"),
@@ -12,13 +13,17 @@ public class Server {
             new User("user5", "1234"),
     };
 
+    // List of currently connected clients
     public static ArrayList<ClientHandler> clients = new ArrayList<>();
 
     public static void main(String[] args) throws Exception {
-        //TODO: start server socket
+        // TODO: Create a ServerSocket listening on a port (e.g., 12345)
 
-        //TODO: accept clients and create a thread for each
-        //TODO: update the list of clients
+        // TODO: Accept incoming client connections in a loop
+        //       For each connection:
+        //       - Create a new ClientHandler object
+        //       - Add it to the 'clients' list
+        //       - Start a new thread to handle communication
     }
 
     public static boolean authenticate(String username, String password) {
